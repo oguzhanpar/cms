53de679fb4251fff0d3cf540af99106347382b9c
oguzhanpar <oguzhan.parlak@yahoo.com>
2020-05-02 02:40:41 +0300

    starting cms pack

---
+++ 1-4-database-tasarimi/cms.sql
@@ -0,0 +1,311 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.5-10.1.16-MariaDB)
+# Database: cms
+# Generation Time: 2017-12-06 20:17:30 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table brands
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `brands`;
+
+CREATE TABLE `brands` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `img_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `rank` int(255) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table courses
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `courses`;
+
+CREATE TABLE `courses` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_turkish_ci,
+  `img_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `event_date` datetime DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table files
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `files`;
+
+CREATE TABLE `files` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `gallery_id` int(11) DEFAULT NULL,
+  `file_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `rank` int(255) DEFAULT NULL,
+  `isActive` tinyint(255) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table galleries
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `galleries`;
+
+CREATE TABLE `galleries` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `gallery_name` varchar(100) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `gallery_type` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `folder_name` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table images
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `images`;
+
+CREATE TABLE `images` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `gallery_id` int(11) DEFAULT NULL,
+  `image_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `rank` int(255) DEFAULT NULL,
+  `isActive` tinyint(255) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table members
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `members`;
+
+CREATE TABLE `members` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `isActive` tinyint(50) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table news
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_turkish_ci,
+  `news_type` varchar(10) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `img_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `video_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table popups
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `popups`;
+
+CREATE TABLE `popups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_turkish_ci,
+  `page` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table product_images
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product_images`;
+
+CREATE TABLE `product_images` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `img_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `isActive` tinyint(11) DEFAULT NULL,
+  `isCover` tinyint(11) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_turkish_ci,
+  `rank` int(11) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table references
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `references`;
+
+CREATE TABLE `references` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_turkish_ci,
+  `img_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table settings
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `settings`;
+
+CREATE TABLE `settings` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `about_us` longtext COLLATE utf8_turkish_ci,
+  `mission` longtext COLLATE utf8_turkish_ci,
+  `vision` longtext COLLATE utf8_turkish_ci,
+  `logo` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `phone_1` varchar(15) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `phone_2` varchar(15) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `fax_1` varchar(15) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `fax_2` varchar(15) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `facebook` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `twitter` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `instagram` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `linkedin` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table slides
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `slides`;
+
+CREATE TABLE `slides` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `allowButton` tinyint(4) DEFAULT NULL,
+  `button_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `button_caption` varchar(25) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `animation_type` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `animation_time` int(11) DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `full_name` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+# Dump of table videos
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `videos`;
+
+CREATE TABLE `videos` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `gallery_id` int(11) DEFAULT NULL,
+  `video_url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `rank` int(255) DEFAULT NULL,
+  `isActive` tinyint(255) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
